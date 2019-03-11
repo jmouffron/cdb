2bbfb9e5b6a7e97f66ee5cb0da8119e557281ebc
jmouffron <jmouffron@excilys.com>
2019-03-11 17:55:01 +0100

    [Feature-Persistence]Start of feature Persistence with creation of DAOFactories and DAOs and config.properties

---
+++ src/com/excilys/cdb/persistence/DaoComputer.java
@@ -0,0 +1,53 @@
+package com.excilys.cdb.persistence;
+
+import java.math.BigInteger;
+import java.sql.Connection;
+import java.util.List;
+
+import com.excilys.cdb.model.Entity;
+
+public class DaoComputer extends DaoInstance {
+
+	@Override
+	Connection getConnection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	List<Entity> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	Entity getOneById(BigInteger id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	Entity create(Entity newEntity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	Entity updateById(BigInteger id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	Entity deleteById(BigInteger id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	Entity deleteByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
